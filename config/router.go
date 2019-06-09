9dd0096e74e9eca8575ce894c81efe26ccb44393
Fernando Balboa <fernando.balboa@mercadolibre.com>
2019-06-09 16:32:38 -0300

    Enable CORS

--- config/router.go
+++ config/router.go
@@ -9,7 +9,14 @@ func ConfiguredRouter() *gin.Engine {
 	router := gin.New()
 
 	//Recover from panics and errors
-	router.Use(apierror.CatchAPIErrors())
+	router.Use(apierror.CatchAPIErrors(), enableCORS())
 
 	return router
+}
+
+//Enable CORS for Angular app
+func enableCORS()gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	}
 }
